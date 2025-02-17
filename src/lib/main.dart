3efff3508e6bc54433f0e000f8fbbf65542e1547
HarshPanchal01 <harshpanchal30x@gmail.com>
2025-02-17 15:27:44 -0500

    PR #1 change request

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:src/screens/home_screen.dart';
+import 'components/main_navbar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomeScreen(),
+      home: const NavigationScaffold(),
     );
   }
 }
--- src/lib/screens/home_screen.dart
+++ src/lib/screens/home_screen.dart
@@ -1,18 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:src/components/main_navbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello Chef Home'),
-        automaticallyImplyLeading: false, // No back button on home screen
-      ),
-      body: const Center(child: Text('Home Screen')),
-      bottomNavigationBar: const BottomNavBar(currentIndex: 1),
+    return const Center(
+      child: Text('Home Screen'),
     );
   }
 }

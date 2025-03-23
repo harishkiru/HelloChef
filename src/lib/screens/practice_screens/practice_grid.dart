41319a3d9ea5f90e729d3b63fd2ef834f9adb12f
HarshPanchal01 <harshpanchal30x@gmail.com>
2025-03-23 12:15:42 -0400

    #27 improvements

--- src/lib/screens/practice_screens/practice_grid.dart
+++ src/lib/screens/practice_screens/practice_grid.dart
@@ -13,19 +13,16 @@ class PracticeGrid extends StatelessWidget {
     }
 
     return GridView.builder(
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(16),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 3,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        childAspectRatio: 0.75,
+        crossAxisCount: 2, // Two items per row
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        childAspectRatio: 0.8, // Adjusted for the new flex ratio
       ),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return Container(
-          color: Colors.transparent,
-          child: TileMaker(item: items[index]),
-        );
+        return TileMaker(item: items[index]);
       },
     );
   }

7ebe507b8e4a5c14194e281a59f9715f361bca94
James Mata <jamesmata556@gmail.com>
2025-04-02 20:18:58 -0400

    stuff

--- src/lib/screens/lesson_screens/videoplayer_screen.dart
+++ src/lib/screens/lesson_screens/videoplayer_screen.dart
@@ -109,7 +109,7 @@ class VideoPlayerScreenState extends State<VideoPlayerScreen> {
               ),
               const SizedBox(height: 8),
               Text(
-                widget.lessonItem.description ??
+                widget.lessonItem.content ??
                     "Watch the video to learn new cooking techniques.",
                 style: TextStyle(fontSize: 14, color: Colors.black87),
               ),
